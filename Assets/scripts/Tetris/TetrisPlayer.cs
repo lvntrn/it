412a593bc900fc9ffe871d77e92b174d9f436885
Le <mail@le-tran.de>
2016-09-18 17:40:05 +0200

    tetris player controller

--- Assets/scripts/Tetris/TetrisPlayer.cs
+++ Assets/scripts/Tetris/TetrisPlayer.cs
@@ -20,8 +20,7 @@ namespace Tetris {
 
 		void FixedUpdate () {
 			float moveHorizontal = Input.GetAxis ("Horizontal");
-			float moveVertical = Input.GetAxis ("Vertical");
-			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
 			rb.AddForce (movement * speed);
 		}
 
