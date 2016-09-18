219452cc44334e4035adcdfa527a036fd84f65c0
Le Tran <mail@le-tran.de>
2016-09-18 02:42:28 +0200

    menu überarbeitet

--- Assets/scripts/Tetris/TetrisPlayer.cs
+++ Assets/scripts/Tetris/TetrisPlayer.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 namespace Tetris {
+	
     public class TetrisPlayer : MonoBehaviour {
+
+		private Rigidbody rb;
+		public float speed;
 
 		public bool  getHit = false;
 		public bool  reduceLive = false;
 
 		public int leftwall = 0;
 		//public int wallsTillWin = 7;
+
+		void Start () {
+			rb = GetComponent<Rigidbody> ();
+		}
+
+		void FixedUpdate () {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.AddForce (movement * speed);
+		}
 
 		void OnTriggerStay(Collider _other) {
 			if (_other.gameObject.tag == "Obstacle") {
@@ -26,11 +41,7 @@ namespace Tetris {
 //			}
 //		}
 
-
-
-		void OnTriggerExit(Collider _other)
-		{
-
+		void OnTriggerExit(Collider _other) {
 			if(_other.gameObject.tag == "Wall") {
 				/*Debug.Log("left wall");*/
 
@@ -41,17 +52,14 @@ namespace Tetris {
 					/*Debug.Log ("no points");*/
 					getHit = false;
 					/*Debug.Log ("wand verlassen");*/
-				} 
-				else {
+				} else {
 					/*Debug.Log ("plus 10punkte");*/
 					TetrisMaster.AddScore(10);
-
 				}
 				if (reduceLive == true) {
 					/*Debug.Log ("reduce live");*/
 					TetrisMaster.reduceLive(1);
 					reduceLive = false;	
-
 				}
 				if (leftwall == 9) {
 					Debug.Log ("Spiel gewonnen Hurra!");
@@ -59,7 +67,8 @@ namespace Tetris {
 				}					
 			}
 		}
-		void Win(){
+
+		void Win() {
 			Application.LoadLevel(4);
 		}
       }				
